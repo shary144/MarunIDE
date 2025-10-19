80b004573f416821e162d31ea5db507d049f087f
southern12014 <12014minami@gmail.com>
2025-10-19 10:38:20 +0900

    moter周りを書き換えました

--- Core/Src/wrapper.cpp
+++ Core/Src/wrapper.cpp
@@ -16,40 +16,52 @@
 const double pi=3.141592;
 
 using namespace std;
-
+//もしボールが接地した状態で動かしたり拾ったりするときボールが逃げないようにするため
+//外側に回転中心を取りたい
 int center[2] = {2,0};
-//とりあえずモーターの回転とかは先に宣言して後からオーバーライド
-int motor(int rotatev, int id){
-    if (rotatev < -100 || rotatev > 100) return -1;
 
-    // PWM変換処理（仮）
-    int pwm = (rotatev + 100) * 10; // 0〜2000の範囲に変換など
-
-    // idに応じてTIMチャネルに出力（例）
-    switch(id){
-        case 0: __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pwm); break;
-        case 1: __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, pwm); break;
-        case 2: __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, pwm); break;
-        default: return -2;
-    }
-
-    return 0;
+//ライントレーサの時と同じ仕組みだとしたらこれ
+//関数motorに、どのくらい回転させるか(speed(-100から100)で指定)、どの車輪を回転させるか(id(0,1,2で指定)送る)を指示する
+int moter(int speed,int id){
+	if(speed > 100) speed=100;
+	if(speed < -100) speed=-100;
+	int PWM = 1500 + (2.5*speed);
+	//iocファイルの"Core" > GPIO_OUTPUT : Configuration > PWM から使っているピンの一覧が見れます
+	if (id==0){
+		//ここはtim3のchannel1にPWMの制御を書き込むという意味;　他も同様に
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, PWM);
+	} else if (id==1){
+		__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, PWM);
+	} else if (id==2){
+		__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, PWM);
+	} else if (id==3){
+		__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, PWM);
+	}
+	return 0;
 }
 
+//どれがどこに対応してるかわからないので基盤読むか実際に動かすかして
+//idがどこに対応するかみてint moter(int id)内のidの振り分けを組み替えてね
+int check_moter(int id){
+		moter(100,id);
+}
+
+//足回りを制御する構造体
 struct UnderCarriage{
 	int init_ang = 0;
 	int rcenter[2]={2,0};//{distance,arg}
-	void handle(int v,int angv/*直進が0,反時計回りに増える*/){
+	//機体本体の舵取り
+	void handle(int v,int angv/*：直進が0,反時計回りに増える*/){
 		/*正射影ベクトルを取るとか*/
 		double x = v*cos(angv*pi/180);
 		double y = v*sin(angv*pi/180);
 		for(int id=0;id<3;id++){
-			int rotatev=-sin((init_ang+120*id)*pi/180)*x+cos((init_ang+120*id)*pi/180)*y;
-			motor(rotatev,id);
+			int speed=-sin((init_ang+120*id)*pi/180)*x+cos((init_ang+120*id)*pi/180)*y;
+			motor(speed,id);
 		}
 	}
 	void rotate(int rotatev){
-		//ここは専ら向きのひずみの修正か射出のための機体転回かあ
+		//ここは専ら向きのひずみの修正か射出のための機体転回
 		for(int id=0;id<3;id++){
 			motor(rotatev,id);
 		}
@@ -59,7 +71,7 @@ struct UnderCarriage{
 extern "C" void main_cpp() {
 	uart_rx_start();
 
-	JoyFrame jf;
+	JoyFrame jf; //これはおそらく先輩が作った別ファイルの構造体
 
     while (1) {
   	  if (read_frame(&jf)) {
